3cd5a034d786d7b53b6e208d005b9138c0b27a1c
alisouran <alirezasoran@gmail.com>
2019-02-02 06:20:15 +0330

    Player 2 Completed

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ function DG(str){
 }
 function init(){
   alert("خب به بازی هزارتو خوش اومدی، برای شروع بازی اول تاس شروع بازی رو بریز و بعد برای هر حرکت یه تاس بریز و شانس خودت رو واس خارج شدن از بازی امتحان کن به این هم دقت کن که تمامی کلیک هات روی دکمه ها به منزله یک حرکت محسوب میشه پس حواست باشی که زیاد کلیک نکنی و برخوردت به دیوار هم حتی جزو حرکت هات محسوب میشه")
-  
-  randomNumber();
   gameMapInputer();
 }
 
@@ -30,7 +28,7 @@ function gameMapInputer(){
   xhttp.send();
 }
 
-var playerCounter = 1;
+var playerCounter = 2;
 
 var counter = 0;
 
@@ -49,14 +47,14 @@ function moveUpEvent(){
             map[i][j] = parseInt(map[i][j])
           }
         }
-        if(playerCounter == 1){
+        if(playerCounter == 1 && parseInt(startRandom1) == 6){
           var player1 = new Player(map);
           player1.moveup();
           if (counter == parseInt(random)){
             playerCounter+=1;
           }
         }
-        else if(playerCounter == 2){
+        else if(playerCounter == 2 && parseInt(startRandom2) == 6){
         var player2 = new Player2(map);
         player2.moveup();
         if (counter == parseInt(random)){
@@ -90,14 +88,14 @@ if (counter < parseInt(random)){
           map[i][j] = parseInt(map[i][j])
         }
       }
-      if(playerCounter == 1){
+      if(playerCounter == 1 && parseInt(startRandom1) == 6){
         var player1 = new Player(map);
         player1.moveright();
         if (counter == parseInt(random)){
           playerCounter+=1;
         }
       }
-      else if(playerCounter == 2){
+      else if(playerCounter == 2 && parseInt(startRandom2) == 6){
       var player2 = new Player2(map);
       player2.moveright();
       if (counter == parseInt(random)){
@@ -131,14 +129,14 @@ function moveLeftEvent(){
             map[i][j] = parseInt(map[i][j])
           }
         }
-        if(playerCounter == 1){
+        if(playerCounter == 1 && parseInt(startRandom1) == 6){
           var player1 = new Player(map);
           player1.moveleft();
           if (counter == parseInt(random)){
             playerCounter+=1;
           }
         }
-        else if(playerCounter == 2){
+        else if(playerCounter == 2 && parseInt(startRandom2) == 6){
         var player2 = new Player2(map);
         player2.moveleft();
         if (counter == parseInt(random)){
@@ -173,14 +171,14 @@ function moveDownEvent(){
             map[i][j] = parseInt(map[i][j])
           }
         }
-        if(playerCounter == 1){
+        if(playerCounter == 1 && parseInt(startRandom1) == 6){
           var player1 = new Player(map);
           player1.movedown();
           if (counter == parseInt(random)){
             playerCounter+=1;
           }
         }
-        else if(playerCounter == 2){
+        else if(playerCounter == 2 && parseInt(startRandom2) == 6){
         var player2 = new Player2(map);
         player2.movedown();
         if (counter == parseInt(random)){
@@ -237,37 +235,73 @@ playerTwo.style.border=0;
 // i.style.marginBottom = '3.5px';
 
 var random;
+var startRandom1;
+var startRandom2;
+var z = false;
+var z2 = false;
 function randomNumber(){
-  DG("upBtn").disabled = false;
-  DG("leftBtn").disabled = false;
-  DG("rightBtn").disabled = false;
-  DG("downBtn").disabled = false;
   counter = 0;
-  if(playerCounter == 1){
-    alert("Player 1")
+  if(playerCounter == 1 && z == false){
+    alert("بازیکن شماره 1 روی تاس شروع بازی کلیک کن")
     playerCounter+=1;
+    console.log("Player Counter = "+playerCounter)
   }
-  else if(playerCounter == 2){
-    alert("Player 2")
+  else if(playerCounter == 2 && z2 == false){
     playerCounter-=1;
+    alert("بازیکن شماره 2 روی تاس شروع بازی کلیک کن")
+    console.log("Player Counter = "+playerCounter)
   }
-  var min = 1;
-  var max = 7;
-  random = Math.random() * (+max - +min) + +min;
-  console.log(parseInt(random));
+  else if (z == true){
+    alert("پلیر 1 تاس بنداز")
+  }
+  else if (z2 == true){
+    alert("پلیر 2 تاس بنداز")
+  }
 }
 
 function startGame(){
-  if(parseInt(random) == 6){
-      alert(parseInt(random)+"آوردی :D")
-      startGameNow();
-      tasRandom();
-      alert("البته برای جایزه‌ی ۶ قبلی"+parseInt(random)+"آوردی :)")
+  var min = 1;
+  var max = 7;
+  if(playerCounter == 1){
+    startRandom1 = Math.random() * (+max - +min) + +min;
+    console.log(parseInt(startRandom1));
+    if(parseInt(startRandom1) == 6){
+      DG("upBtn").disabled = false;
+      DG("leftBtn").disabled = false;
+      DG("rightBtn").disabled = false;
+      DG("downBtn").disabled = false;
+        alert(parseInt(startRandom1)+"آوردی :D")
+        startGameNow();
+        tasRandom();
+        alert("البته برای جایزه‌ی ۶ قبلی"+parseInt(random)+"آوردی :)")
+        z2 = true;
+    }
+    else {
+      alert(parseInt(startRandom1)+"آوردی :(")
+      alert("متاسفانه برای شروع بازی ۶ نیاوردی");
+      randomNumber();
+      console.log("Counter = "+playerCounter)
+    }
   }
-  else {
-    alert(parseInt(random)+"آوردی :(")
-    alert("متاسفانه برای شروع بازی ۶ نیاوردی");
-    randomNumber();
+  else if(playerCounter == 2){
+    startRandom2 = Math.random() * (+max - +min) + +min;
+    console.log(parseInt(startRandom2));
+    if(parseInt(startRandom2) == 6){
+      DG("upBtn").disabled = false;
+      DG("leftBtn").disabled = false;
+      DG("rightBtn").disabled = false;
+      DG("downBtn").disabled = false;
+        alert(parseInt(startRandom2)+"آوردی :D")
+        startGameNow();
+        tasRandom();
+        alert("البته برای جایزه‌ی ۶ قبلی"+parseInt(random)+"آوردی :)")
+        z = true;
+    }
+    else {
+      alert(parseInt(startRandom2)+"آوردی :(")
+      alert("متاسفانه برای شروع بازی ۶ نیاوردی");
+      randomNumber();
+    }
   }
 }
 
@@ -279,13 +313,14 @@ function startGameNow(){
     DG("pr"+p+d).appendChild(playerOne);
   }
   else if(playerCounter == 2){
-    DG("pr"+p+d).appendChild(playerTwo);
+    DG("pr"+q+s).appendChild(playerTwo);
   }
 }
 
 function resetGame(){
   location.reload(true);
 }
+
 
 class Player {
   constructor(map) {
@@ -306,6 +341,14 @@ class Player {
       console.log(p+","+d);
       alert("به دیوار رسیدی");
       p+=1;
+    }
+    else if(q==p && d==s){
+      var deleteElement = playerTwo;
+      deleteElement.parentNode.removeChild(deleteElement);
+      q=0;
+      s=0;
+      console.log(p+","+d);
+      DG("pr"+p+d).appendChild(playerOne);
     }
     else{
       console.log(p+","+d);
@@ -329,6 +372,14 @@ class Player {
       alert("به دیوار رسیدی");
       d-=1;
     }
+    else if(q==p && d==s){
+      var deleteElement = playerTwo;
+      deleteElement.parentNode.removeChild(deleteElement);
+      q=0;
+      s=0;
+      console.log(p+","+d);
+      DG("pr"+p+d).appendChild(playerOne);
+    }
     else{
       console.log(p+","+d);
       DG("pr"+p+d).appendChild(playerOne);
@@ -350,6 +401,14 @@ class Player {
       alert("به دیوار رسیدی");
       d+=1;
     }
+    else if(q==p && d==s){
+      var deleteElement = playerTwo;
+      deleteElement.parentNode.removeChild(deleteElement);
+      q=0;
+      s=0;
+      console.log(p+","+d);
+      DG("pr"+p+d).appendChild(playerOne);
+    }
     else{
       console.log(p+","+d);
       DG("pr"+p+d).appendChild(playerOne);
@@ -370,6 +429,14 @@ class Player {
     else if (this.map[p][d]==1) {
       alert("به دیوار رسیدی");
       p-=1;
+    }
+    else if(q==p && d==s){
+      var deleteElement = playerTwo;
+      deleteElement.parentNode.removeChild(deleteElement);
+      q=0;
+      s=0;
+      console.log(p+","+d);
+      DG("pr"+p+d).appendChild(playerOne);
     }
     else{
       console.log(p+","+d);
@@ -400,6 +467,14 @@ class Player2 {
       alert("به دیوار رسیدی");
       q+=1;
     }
+    else if(q==p && d==s){
+      var deleteElement = playerOne;
+      deleteElement.parentNode.removeChild(deleteElement);
+      p=0;
+      d=0;
+      console.log(q+","+s);
+      DG("pr"+q+s).appendChild(playerTwo);
+    }
     else{
       console.log(q+","+s);
       DG("pr"+q+s).appendChild(playerTwo);
@@ -422,6 +497,14 @@ class Player2 {
       alert("به دیوار رسیدی");
       s-=1;
     }
+    else if(q==p && d==s){
+      var deleteElement = playerOne;
+      deleteElement.parentNode.removeChild(deleteElement);
+      p=0;
+      d=0;
+      console.log(q+","+s);
+      DG("pr"+q+s).appendChild(playerTwo);
+    }
     else{
       console.log(q+","+s);
       DG("pr"+q+s).appendChild(playerTwo);
@@ -443,6 +526,14 @@ class Player2 {
       alert("به دیوار رسیدی");
       s+=1;
     }
+    else if(q==p && d==s){
+      var deleteElement = playerOne;
+      deleteElement.parentNode.removeChild(deleteElement);
+      p=0;
+      d=0;
+      console.log(q+","+s);
+      DG("pr"+q+s).appendChild(playerTwo);
+    }
     else{
       console.log(q+","+s);
       DG("pr"+q+s).appendChild(playerTwo);
@@ -463,6 +554,14 @@ class Player2 {
     else if (this.map[q][s]==1) {
       alert("به دیوار رسیدی");
       q-=1;
+    }
+    else if(q==p && d==s){
+      var deleteElement = playerOne;
+      deleteElement.parentNode.removeChild(deleteElement);
+      p=0;
+      d=0;
+      console.log(q+","+s);
+      DG("pr"+q+s).appendChild(playerTwo);
     }
     else{
       console.log(q+","+s);
